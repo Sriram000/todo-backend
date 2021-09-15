3c8b3dd308db818a3e66ea064236e4680ee2e8b2
srm-laufire <srm@laufire.in>
2021-09-15 14:07:44 +0000

    Introduced CORS headers.

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const todoRouter = require("./router/todoRouter");
 const taskRouter = require("./router/taskRouter");
 
 const app = express();
-const PORT = 3500;
+const PORT = 5000;
 
 app.use(express.urlencoded());
 app.use(express.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use('/todo', todoRouter);
 app.use('/task', taskRouter);
